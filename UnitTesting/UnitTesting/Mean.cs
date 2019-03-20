cf301d5ae00bc8503eb3a65d566174d2ea3d237f
Mario <32181249+MarioAGtzC@users.noreply.github.com>
2019-03-20 09:53:34 -0600

    Almacenar los casos de prueba

---
+++ UnitTesting/UnitTesting/Mean.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace UnitTesting
+{
+    class Mean
+    {
+        public static double arithmeticMean(params int[] list)
+        {
+            double sum = 0;
+            for (int i = 0; i < list.Length; i++)
+                sum += list[i];
+            return sum / list.Length;
+        }
+
+        public static double geometricMean(params int[] list)
+        {
+            double mult = 1;
+            for(int i = 0; i < list.Length; i++)
+                mult *= list[i];
+            double pow = 1.0 / list.Length;
+            return Math.Pow(mult, pow);
+        }
+
+        public static double harmonicMean(params int[] list)
+        {
+            double sum = 0;
+            for(int i = 0; i < list.Length; i++)
+            {
+                sum += (1.0 / list[i]);
+            }
+            return list.Length / sum;
+        }
+    }
+}
---
+++ UnitTesting/UnitTesting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnitTesting
+{
+    class Program
+    {
+        Mean medias = new Mean();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Mean.arithmeticMean(1, 43, 5, 7, 8));
+            Console.WriteLine(Mean.geometricMean(9, 78, 54));
+            Console.WriteLine(Mean.harmonicMean(3, 4, 5));
+            Console.ReadKey();
+        }
+    }
+}
