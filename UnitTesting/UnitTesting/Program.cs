c018100fb118a3d35430280bb735e3ec3b31175b
Mario <32181249+MarioAGtzC@users.noreply.github.com>
2019-03-21 11:39:24 -0600

    Problema 02: Leer los casos de prueba

--- UnitTesting/UnitTesting/Program.cs
+++ UnitTesting/UnitTesting/Program.cs
@@ -5,13 +5,12 @@ namespace UnitTesting
     class Program
     {
         Mean medias = new Mean();
+        ReadFile rf = new ReadFile();
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(Mean.arithmeticMean(1, 43, 5, 7, 8));
-            Console.WriteLine(Mean.geometricMean(9, 78, 54));
-            Console.WriteLine(Mean.harmonicMean(3, 4, 5));
+            Console.WriteLine(ReadFile.readFile());
             Console.ReadKey();
         }
     }
---
+++ UnitTesting/UnitTesting/ReadFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace UnitTesting
+{
+    class ReadFile
+    {
+        public static string readFile()
+        {
+            StreamReader sr = new StreamReader("CasosPrueba.txt");
+            string line = sr.ReadToEnd();
+            return line;
+        }
+    }
+}
